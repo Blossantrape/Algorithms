e3038f51f2717a10ac097aa3d9cc2a1e074c2608
Blossantrape <tolik.hamer@gmail.com>
2022-12-10 16:29:52 +0300

    Minor fix

---
+++ AddClass.cs
@@ -0,0 +1,33 @@
+namespace Algorithms
+{
+    class AddClass
+    { 
+        public int? BinarySearch(List<int> list, int item)
+        {
+            
+            int low = 0;
+            int high = list.Count - 1;
+            
+            while (low <= high)
+            {
+                int temp = (low + high) / 2;
+                int mid = Convert.ToInt32(temp);
+                int guess = list[mid];
+
+                if (guess == item)
+                {
+                    return mid;
+                }
+                else if (guess > item)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ MainClass.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            AddClass search = new AddClass();
+            
+            List<int> numbers = new List<int>() {1, 3, 5, 7, 9};
+            
+            Console.Write("Ведите значение для поиска: ");
+            string? inputNumberToConvert = Console.ReadLine();
+            int inputNumber = Convert.ToInt32(inputNumberToConvert);
+            
+            Console.WriteLine($"Элемент {inputNumber} под индексом {search.BinarySearch(numbers, inputNumber) }");
+        }
+    }
+}
